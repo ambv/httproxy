63f88f6cce1dd29b1e7d4d1fea49567df245e971
Łukasz Langa <lukasz@langa.pl>
2012-10-25 14:48:30 +0200

    PEP8-ified the source

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 import os
 import sys
 from setuptools import setup, find_packages
